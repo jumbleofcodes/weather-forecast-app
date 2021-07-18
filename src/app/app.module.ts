52b7cae0de7c7f1ff653aae3a5c774ce6dda86ad
jumbleofcodes <45831879+jumbleofcodes@users.noreply.github.com>
2021-07-18 19:08:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LeafletModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
